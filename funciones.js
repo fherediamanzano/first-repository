401f9d160cb6ef42806be9bd15aa85d0f8850dd0
fherediamanzano <manzano.francisco.heredia@gmail.com>
2022-02-03 00:17:58 -0300

    nuevos archivos, practica Json

--- funciones.js
+++ funciones.js
@@ -1,1 +1,9 @@
-alert("Hola Mundo")
+/*alert("Hola Mundo")
+const deportista={
+    nombre: "Messi",
+    champions: 4,
+
+};
+console.log(JSON.stringify(deportista));
+*/
+fetch('http://127.0.0.1:5500/pasajero.json').then((response) => response.json()).then(data => console.log(data));
